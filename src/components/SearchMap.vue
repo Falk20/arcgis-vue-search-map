9f9a697c88a089feaecb55c6d314ae8535b3924e
Falk <petruk0908@gmail.com>
2020-07-23 22:48:15 +0300

    add map on component

---
+++ src/components/SearchMap.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="search-map"></div>
+</template>
+
+<script>
+import { loadMap } from "../mapSetting";
+
+export default {
+  name: "search-map",
+  mounted() {
+    // lazy load the required ArcGIS API for JavaScript modules and CSS
+    loadMap(this);
+  },
+  beforeDestroy() {
+    if (this.view) {
+      // destroy the map view
+      this.view.container = null;
+    }
+  },
+};
+</script>
+
+<style lang="scss">
+.search-map {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
